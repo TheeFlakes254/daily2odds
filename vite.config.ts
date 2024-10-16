053372d112564453c0123bf5c121946fb9a399a4
TheeFlakes254 <mmdennis25@gmail.com>
2024-10-16 13:40:39 +0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
-    fs: {
-      allow: ['.'], // This allows serving files from the entire project directory
-    },
-  },
-  optimizeDeps: {
-    exclude: ['your-problematic-module'] // Replace with the name of the module causing issues, if known
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000
   }
 });
