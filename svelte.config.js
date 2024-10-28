b11abb697476e9fd088dd1f673a7ff53c650ba7b
TheeFlakes254 <mmdennis25@gmail.com>
2024-10-28 20:48:19 +0300

    updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
+// svelte.config.js
 import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter()
-    }
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        adapter: adapter(),
+        csrf: {
+            checkOrigin: false,
+        }
+    },
+    preprocess: vitePreprocess()
 };
 
 export default config;
