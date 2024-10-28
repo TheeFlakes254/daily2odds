2fdc51be41792c4fbc67c1a630f2d24a04cf75df
TheeFlakes254 <mmdennis25@gmail.com>
2024-10-28 21:02:29 +0300

    Fix deployment configuration and dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,14 @@
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
       out: 'build',
       precompress: false,
       envPrefix: ''
     })
-  },
-  preprocess: vitePreprocess()
+  }
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
-    host: '0.0.0.0',
+    host: true,
     port: process.env.PORT || 3000
   }
 });
